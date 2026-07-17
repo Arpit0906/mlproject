ac97bec8ed363e5c3447178b39083390469233aa
Arpit <arpitgangwani517@gmail.com>
2026-07-17 06:47:37 +0530

    Hyperparameter Tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -46,7 +46,44 @@ class ModelTrainer:
             "CatBoosting Regressor": CatBoostRegressor(verbose=False),
             "AdaBoost Regressor": AdaBoostRegressor()
             }
-            model_report:dict=evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+            params={
+                "Decision Tree":{
+                    "criterion": ["squared_error", "friedman_mse", "absolute_error", "poisson"],
+                    "splitter": ["best", "random"],
+                    "max_features": ["sqrt", "log2", None]
+                },
+                "Random Forest Regressor":{
+                    "n_estimators": [100, 200, 300],
+                    "max_features": ["sqrt", "log2", None],
+                    "criterion": ["squared_error", "friedman_mse", "absolute_error", "poisson"]
+                },
+                "Gradient Boosting":{    
+                    "learning_rate": [0.1, 0.01, 0.05, 0.001],
+                    "subsample": [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    "n_estimators": [100, 200, 300],
+                    "max_depth": [3, 4, 5, 6]
+                },
+                "Linear Regression":{},
+                "K-Neighbors Regressor":{
+                    #"weight": ["uniform", "distance"],
+                    "n_neighbors": [3, 5, 7, 9]
+                },
+                "XGBRegressor":{
+                    "learning_rate": [0.1, 0.01, 0.05, 0.001],
+                    "n_estimators": [100, 200, 300],
+                    "max_depth": [3, 4, 5, 6]        
+                     },
+                "CatBoosting Regressor":{
+                    "depth": [6, 8, 10],
+                    "learning_rate": [0.01, 0.05, 0.1],
+                    "iterations": [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    "learning_rate": [0.1, 0.01, 0.05, 0.001],
+                    "n_estimators": [50, 100, 200]
+                }
+            }
+            model_report:dict=evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,params=params)
 
             ## To get best model score from dict
             best_model_score = max (sorted (model_report.values()))
@@ -66,8 +103,8 @@ class ModelTrainer:
                 file_path=self.model_trainer_config.trained_model_file_path,
                 obj=best_model
             )
-
-            predicted=best_model. predict(X_test)
+            best_model.fit(X_train, y_train)
+            predicted=best_model.predict(X_test)
             r2_square = r2_score(y_test, predicted)
             return r2_square
         except Exception as e:
